0c20dcf17620259ecaa773bb9b47c6a70e9ca18d
vunky-himawan <tingkerbeii44@gmail.com>
2024-05-14 10:33:20 +0700

    Jobsheet 12: B. Informasi File Upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,6 +14,21 @@ class FileUploadController extends Controller
 
     public function prosesFileUpload(Request $request)
     {
-        return "Pemrosesan file upload di sini";
+        // dump($request->berkas);
+        if ($request->hasFile('berkas')) {
+            echo "path() = " . $request->file('berkas')->path();
+            echo "<br>";
+            echo "extension() = " . $request->file('berkas')->extension();
+            echo "<br>";
+            echo "getClientOriginalExtension() = " . $request->file('berkas')->getClientOriginalExtension();
+            echo "<br>";
+            echo "getMimeType() = " . $request->file('berkas')->getMimeType();
+            echo "<br>";
+            echo "getClientOriginalName() = " . $request->file('berkas')->getClientOriginalName();
+            echo "<br>";
+            echo "getSize() = " . $request->file('berkas')->getSize();
+        } else {
+            echo "Tidak ada berkas yang diupload";
+        }
     }
 }
